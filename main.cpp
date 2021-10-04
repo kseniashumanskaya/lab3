c2066142c924154cfadfe6bae5ed21667a5a214c
KseniaShumanskaya <KseniaShumanskaya@gmail.com>
2021-10-04 17:52:35 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,126 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int TheNumberOfTheFirstRowContaining0 (vector <vector<int>>vec, int size) {
+    int m, num;
+    for (int i = 0; i < size; i++) {
+        m = 0;
+        num = -1;
+        for (int j = 0; j < size; j++) {
+            if (vec[i][j] == 0)
+                m++;
+        }
+        if (m > 0)
+        {
+            num = i;
+            break;
+        }
+
+    }
+    return num;
+}
+
+int main() {
+
+    int size;
+    cout << "enter the size of the matrix:" << endl;
+    cin >> size;
+
+    cout << " enter 0 if you want to fill the matrix with random numbers and 1 - from consol" << endl;
+    int matrix_filling_option;
+    cin >> matrix_filling_option;
+    vector <vector<int>> vec;
+    if (matrix_filling_option == 0) {
+        srand(time(0));
+        for (int i = 0; i < size; i++) {
+            vector <int> temp;
+            for (int j = 0; j < size; j++) {
+
+                temp.push_back(rand() % 20 - 10);
+            }
+            vec.push_back(temp);
+        }
+    }
+
+    else {
+        cout << "enter matrix elements:" << endl;
+
+        for (int i = 0; i < size; i++) {
+            vector <int> temp;
+            int n;
+            for (int j = 0; j < size; j++) {
+                cin >> n;
+                temp.push_back(n);
+            }
+            vec.push_back(temp);
+        }
+    }
+   
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++)
+            vec[i][j] = vec[j][i];
+    }
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++)
+        {
+            cout << vec[i][j] << "  ";
+        }
+        cout << endl;
+    }
+    
+    if (TheNumberOfTheFirstRowContaining0(vec, size) < 0)
+        cout << " there is no zero element in the matrix  " << endl;
+    else
+        cout << "the number of the first row containing 0 : " << TheNumberOfTheFirstRowContaining0(vec, size) + 1 << endl;
+
+    int s = 0;
+    for (int i = 0; i < vec.size(); i++) {
+        for (int j = 0; j < vec.size(); j++) {
+            s += abs(vec[i][j]);
+        }
+    }
+
+    if (s==0) {
+        for (int i = 0; i < vec.size(); i++) {
+            for (int j = 0; j < vec.size(); j++) {
+                vec.pop_back();
+            }
+        }
+        cout << "matrix consisted of only zeros" << endl;
+    }
+
+    else {
+        for (int k = 0; k < vec.size(); k++) {
+            for (int i = 0; i < vec.size(); i++) {
+                int s = 0;
+                for (int j = 0; j < vec.size(); j++) {
+                    s += abs(vec[i][j]);
+                }
+                if (s == 0) {
+                    int  a = i;
+                    while (i < vec.size() - 1) {
+                        for (int j = 0; j < vec.size(); j++)
+                            vec[i][j] = vec[i + 1][j];
+                        i++;
+                    }
+                    for (int j = a; j < vec.size() - 1; j++) {
+                        for (int i = 0; i < vec.size(); i++)
+                            vec[i][j] = vec[i][j + 1];
+                    }
+                    vec.pop_back();
+                    k--;
+                }
+            }
+        }
+        for (int i = 0; i < vec.size(); i++) {
+            for (int j = 0; j < vec.size(); j++)
+            {
+                cout << vec[i][j] << "  ";
+            }
+            cout << endl;
+        }
+    }
+}
+
